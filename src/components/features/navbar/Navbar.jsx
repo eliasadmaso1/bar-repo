2b11b5dc870a5fd33312e1fdfc98e305c734a423
eliasadmaso1 <eliasadmas01@gmail.com>
2022-09-01 18:31:42 +0300

    deploy

--- src/components/features/navbar/Navbar.jsx
+++ src/components/features/navbar/Navbar.jsx
@@ -1,130 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import "./navbar.css";
-import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
-import FacebookIcon from "@mui/icons-material/Facebook";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
-import CancelIcon from "@mui/icons-material/Cancel";
-import logo from "../../../images/logo.png";
+import CloseIcon from '@mui/icons-material/Close';
+import { useRef } from "react";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 
-function Navbar() {
-  const [click, setClick] = useState(false);
 
-  const handleClick = () => {
-    setClick(!click);
-  };
-  const menuData = [
-    {
-      title: <LocalPhoneIcon />,
-      href: "tel:0552753626",
-    },
-    {
-      title: <FacebookIcon />,
-      href: "https://www.facebook.com/daniel.masala.14",
-    },
-    {
-      title: "ביקורות",
-      link: "/feedback",
-    },
-    {
-      title: "גלריה",
-      link: "/gallery",
-    },
-    {
-      title: "תפריט",
-      link: "/menu",
-    },
-    {
-      title: "אודות",
-      link: "/about",
-    },
-    {
-      title: "בית",
-      link: "/bar-repo",
-    },
-  ];
 
-  const menuMobileData = [
-    {
-      title: "בית",
-      link: "/bar-repo",
-    },
-    {
-      title: "אודות",
-      link: "/about",
-    },
-    {
-      title: "תפריט",
-      link: "/menu",
-    },
-    {
-      title: "גלריה",
-      link: "/gallery",
-    },
-    {
-      title: "ביקורות",
-      link: "/feedback",
-    },
-    {
-      title: <FacebookIcon />,
-      href: "https://www.facebook.com/daniel.masala.14",
-    },
-    {
-      title: <LocalPhoneIcon />,
-      href: "tel:0552753626",
-    },
-  ];
+const Navbar = () => {
+
+  const navRef = useRef();
+
+  const handleChange = ()=>{
+      navRef.current.classList.toggle('responsive-navbar');
+  }
+
   return (
-    <>
-      <div className="navbar-container">
-     
+    <header>
+      <nav ref={navRef}>
+        <Link to="/bar-repo" onClick={handleChange}>בית</Link>
+        <Link to="/about" onClick={handleChange}>אודות</Link>
+        <Link to="/menu" onClick={handleChange}>תפריט</Link>
+        <Link to="/" onClick={handleChange}>גלריה</Link>
+        <Link to="/feedback" onClick={handleChange}>ביקורות</Link>
+        <Link to="/" onClick={handleChange}><WhatsAppIcon/></Link>
+        <Link to="/" onClick={handleChange}><FacebookOutlinedIcon/></Link>
 
-        {menuData.map((item) => {
-          return item.link ? (
-            <Link to={item.link}>
-              <div className="menu-item-container">
-                <h4>{item.title}</h4>
-              </div>
-            </Link>
-          ) : (
-            <a href={item.href}>
-              <div className="menu-item-container">
-                <h4>{item.title}</h4>
-              </div>
-            </a>
-          );
-        })}
-      </div>
-      <div className="nav">
-        <div className="nav-container">
-        <img
-            className="logo"
-            src={logo}
-          />
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <CancelIcon /> : <MenuIcon />}
-          </div>
-          <ul className={click ? "nav-menu-active" : "nav-menu"}>
-            {menuMobileData.map((item) => {
-              return item.link ? (
-                <li onClick={handleClick}>
-                  <Link to={item.link} className="media-link">
-                    {item.title}
-                  </Link>
-                </li>
-              ) : (
-                <li onClick={handleClick}>
-                  <a href={item.href} className="media-link">
-                    {item.title}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-    </>
+
+
+        <button onClick={handleChange} className="nav-btn nav-close-btn">
+          <CloseIcon style={{fontSize:"25px"}}/>
+        </button>
+      </nav>
+      <button onClick={handleChange} className="nav-btn">
+        <MenuIcon style={{fontSize:"25px"}}/>
+      </button>
+    </header>
   );
-}
+};
 
 export default Navbar;
